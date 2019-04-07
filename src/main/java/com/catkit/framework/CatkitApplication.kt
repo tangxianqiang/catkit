97686295a0e812a3e360e4e246a2ef57ff059eed
tangxianqiang <2927001935@qq.com>
2019-04-08 00:27:02 +0800

    add base application and crasher

---
+++ src/main/java/com/catkit/framework/CatkitApplication.kt
@@ -0,0 +1,13 @@
+package com.catkit.framework
+
+import android.app.Application
+
+/**
+ * Base class of application.
+ * @author      XianQiang Tang
+ * @see         Application
+ * @version     1.0.0
+ */
+abstract class CatkitApplication : Application() {
+
+}
---
+++ src/main/java/com/catkit/framework/crash/AppCrashHandler.kt
@@ -0,0 +1,32 @@
+package com.catkit.framework.crash
+
+import android.app.Application
+
+/**
+ * Thread catch exception if it has its own UncaughtExceptionHandler.
+ * Or ThreadGroup will do it. Otherwise it done by default.
+ * <p>This class is instanced in Application. AppCrashHandler only writes
+ * errors to local file not sends to server.
+ * </p>
+ * @author      XianQiang Tang
+ * @see         Thread.UncaughtExceptionHandler
+ * @version     1.0.0
+ */
+internal class AppCrashHandler(private val appCtx: Application) : Thread.UncaughtExceptionHandler {
+    init {
+        Thread.setDefaultUncaughtExceptionHandler(this)
+    }
+
+    companion object {
+        /**
+         * Init instance and to be sure init method called.
+         */
+        fun initCrasher(app: Application) {
+            AppCrashHandler(app)
+        }
+    }
+
+    override fun uncaughtException(t: Thread?, e: Throwable?) {
+
+    }
+}
